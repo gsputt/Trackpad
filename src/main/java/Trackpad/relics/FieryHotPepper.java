99fa767f372d3bef3d26e41f267d049c693ade43
gsputt <gsputt@gmail.com>
2019-09-16 01:01:18 +0700

    Changed the effect of Fiery Hot Pepper as to not clash with vex mod

--- src/main/java/Trackpad/relics/FieryHotPepper.java
+++ src/main/java/Trackpad/relics/FieryHotPepper.java
@@ -3,9 +3,13 @@ package Trackpad.relics;
 import Trackpad.trackpad;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
-import com.megacrit.cardcrawl.actions.utility.QueueCardAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.red.FireBreathing;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.status.Burn;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
@@ -16,17 +20,24 @@ public class FieryHotPepper extends CustomRelic {
     public static final String OUTLINE = trackpad.makePath(trackpad.FIERY_HOT_PEPPER_OUTLINE);
 
     public FieryHotPepper() {
-        super(ID, new Texture(IMG), new Texture(OUTLINE), RelicTier.COMMON, LandingSound.HEAVY);
+        super(ID, new Texture(IMG), new Texture(OUTLINE), RelicTier.RARE, LandingSound.HEAVY);
     }
 
     @Override
     public void atBattleStart()
     {
-        AbstractCard FireBreathing = new FireBreathing();
-        FireBreathing.applyPowers();
-        FireBreathing.freeToPlayOnce = true;
-        FireBreathing.purgeOnUse = true;
-        AbstractDungeon.actionManager.addToBottom(new QueueCardAction(FireBreathing, AbstractDungeon.getRandomMonster()));
+        AbstractCard burnCard = new Burn();
+        AbstractDungeon.actionManager.addToTop(new MakeTempCardInDrawPileAction(burnCard, 2, true, true));
+    }
+
+    @Override
+    public void onCardDraw(AbstractCard drawnCard) {
+        if(drawnCard.cardID.equals(Burn.ID))
+        {
+            AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction((AbstractCreature)null, DamageInfo.createDamageMatrix(8, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE, true));
+            AbstractDungeon.player.hand.moveToDiscardPile(drawnCard);
+            this.flash();
+        }
     }
 
     @Override
